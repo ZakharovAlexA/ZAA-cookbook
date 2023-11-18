0597daf6f132a4b939298d8cf3ed16eaeab6f909
ZakharovAlexA <123879443+ZakharovAlexA@users.noreply.github.com>
2023-11-18 23:17:31 +0700

    Update settings.py

--- cookbook_backend/cookbook_backend/cookbook_backend/settings.py
+++ cookbook_backend/cookbook_backend/cookbook_backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tk(b#_f96h!)(=io3+_2co@@)je8jg%q9a)%l)096d%0tm-g#-'
+SECRET_KEY = 'django-insecure*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
